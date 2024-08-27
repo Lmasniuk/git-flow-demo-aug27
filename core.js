d18a8937ff090ce15225a80da026fb6880fcd74f
Logan Masniuk <logan.masniuk@gmail.com>
2024-08-27 11:01:01 -0700

    Testing develop branch protection

--- core.js
+++ core.js
@@ -3,4 +3,4 @@ console.log("This is the main file of the project");
 const question = "Why are teenagers so odd?";
 const answer = "Because they can't even!";
 
-const foo = "test";
+const foo = "test2";
